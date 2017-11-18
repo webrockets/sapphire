72b2ac1277a7af9fac210d18d38a55362238a83e
Marco Tulio de Avila Santos <marcotulio.avila@gmail.com>
2017-11-18 12:03:53 -0200

    add and configure devise

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 gem 'bcrypt', '~> 3.1.7'
 gem 'coffee-rails', '~> 4.2'
+gem 'devise'
 gem 'jbuilder', '~> 2.5'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.7'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,18 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_HOST'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       ENV['SMTP_AUTHENTICATION'],
+    enable_starttls_auto: true
+  }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
