6bced7e46a5afaaa5766f73b3c833782ddeb4092
Marco Tulio de Avila Santos <marcotulio.avila@gmail.com>
2017-12-03 00:04:59 -0200

    add component helper in application helper
    This helper is a easier wrapper to the rails render method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def component(name, params = {}, &block)
+    render_component(name, params, &block)
+  end
+
+  private
+
+  def render_component(name, locals, &block)
+    return render(layout: name, locals: locals, &block) if block_given?
+    render partial: name, locals: locals
+  end
 end
