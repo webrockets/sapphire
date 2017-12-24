bb1b8ba7c9ec1dcfbab97adfed147f575d121dfb
Marco Tulio de Avila Santos <marco@devsquad.com>
2017-12-24 17:49:56 -0200

    fixed Vue load, add SelectRole removed UserList

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,40 +1,59 @@
 /* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
 // third party libraries
 import Vue from 'vue/dist/vue.esm'
 import axios from 'axios'
 import Toaster from '../services/toaster'
 
 // Application components
-import UserList from '../users/UserList'
+import SelectRole from '../users/SelectRole'
 import Notification from '../shared/notification'
 
 // Add application components here
 const components = {
-  UserList,
+  SelectRole,
   Notification
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+const loadVue = () => {
   const header = document.getElementsByName('csrf-token')[0].getAttribute('content')
   axios.defaults.headers.common = {
     'X-CSRF-TOKEN': header,
     'X-Requested-With': 'XMLHttpRequest'
   };
 
-  Vue.prototype.$routes = Routes
+  axios.interceptors.response.use(null, (error) => {
+    console.error('HTTP ERROR:', error);
+    if (error.response) {
+      const response = error.response
+
+      if (response.data && response.data.message) {
+        const toaster = new Toaster()
+        const message = response.data.message
+        toaster.error({ message })
+      }
+
+      return Promise.reject(response)
+    }
+    return Promise.reject(error)
+  })
+
+  Vue.prototype.$route = Routes
   Vue.prototype.$http = axios
   Vue.prototype.$toaster = new Toaster
+  Vue.prototype.$current_user = null
 
   const app = new Vue({
     el: '#app',
-    components
+    components,
+    created() {
+      this.$http.get(this.$route.api_current_user_path()).then(
+        response => {
+          this.$current_user = response.data
+        }
+      )
+    }
   })
-})
+}
+
+document.addEventListener('turbolinks:load', loadVue)
+// document.addEventListener('DOMContentLoaded', loadVue)
---
+++ app/javascript/users/SelectRole.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="select" :class="{'is-loading': loading}">
+    <select v-model="selected_role" @change="changeRole">
+      <option v-for="(option, index) in options" :key="index" :value="option[1]">{{ option[0] }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    userId: { required: true },
+    role: { required: true, type: String },
+    options: { default: () => [] },
+  },
+
+  data() {
+    return {
+      selected_role: null,
+      previous_role: null,
+      loading: false,
+    }
+  },
+
+  created() {
+    this.selected_role = this.role
+    this.previous_role = this.role
+  },
+
+  methods: {
+    changeRole() {
+      this.loading = true
+      this.$http.put(this.$route.api_user_change_role_path(this.userId), { role: this.selected_role })
+        .then(this.onChangeSuccess.bind(this), this.onChangeFailed.bind(this))
+    },
+
+    onChangeSuccess() {
+      this.loading = false
+      this.$toaster.success({ message: 'Perfil alterado com sucesso' })
+      window.location.reload();
+    },
+
+    onChangeFailed(error) {
+      this.loading = false
+      this.selected_role = this.previous_role
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.select-role
+  color: red
+</style>
