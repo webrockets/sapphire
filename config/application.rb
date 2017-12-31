11a71f37e0329af1869abfeed7eaaa3c0c81bd2c
Marco Tulio de Avila Santos <marco@devsquad.com>
2017-12-31 13:45:47 -0200

    modularize i18n to separate files

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Sapphire
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     I18n.available_locales = %i[en pt_BR]
     I18n.default_locale = :pt_BR
 
